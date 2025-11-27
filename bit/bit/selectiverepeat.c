d29e546d5332be049eac3631968d0218f11b4fa4
Navyasruthi-yerramsetti <151709424+Navyasruthi-yerramsetti@users.noreply.github.com>
2025-11-28 04:50:20 +0530

    selectiverepeat.c

---
+++ bit/bit/selectiverepeat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int total_frames, window_size;
+    int i = 1, j, ack;
+
+    printf("Enter total number of frames to send: ");
+    scanf("%d", &total_frames);
+
+    printf("Enter window size: ");
+    scanf("%d", &window_size);
+
+    printf("\n--- Selective Repeat ARQ Simulation ---\n");
+
+    while (i <= total_frames) {
+        // Send frames in the current window
+        printf("\nSending frames: ");
+        for (j = i; j < i + window_size && j <= total_frames; j++) {
+            printf("%d ", j);
+        }
+
+        // Acknowledgment checking
+        for (j = i; j < i + window_size && j <= total_frames; j++) {
+            printf("\nDid you receive ACK for frame %d? (1 = Yes, 0 = No): ", j);
+            scanf("%d", &ack);
+
+            if (ack == 0) {
+                printf("Resending frame %d...\n", j);
+                printf("Frame %d received successfully now.\n", j);
+            } else {
+                printf("Frame %d acknowledged successfully.\n", j);
+            }
+        }
+
+        // Move to next set of frames
+        i = i + window_size;
+    }
+
+    printf("\nAll frames sent and acknowledged successfully.\n");
+    return 0;
+}
